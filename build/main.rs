23a5fcfd41e3a8909f5dcb1afbd004726378273e
whs <hswongac@gmail.com>
2024-01-04 08:11:40 +0800

    Test

--- build/main.rs
+++ build/main.rs
@@ -1,19 +1,34 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-use copy_dir::copy_dir;
 use cudart_sys::{cuda_lib_path, cuda_path};
 use std::env;
+use std::{io, fs};
 
 mod gates;
 mod poseidon_constants;
 mod template;
 
+fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    let out_path = std::path::PathBuf::from(env::var("OUT_DIR").unwrap());
-    let binding = out_path.join("native");
-    let test = binding.to_str().expect("REASON");
-    copy_dir("./native", test).expect("Could not copy");
+    let out_path = std::path::PathBuf::from("./native");
+    let out_path2 = std::path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    let binding2 = out_path2.join("native");
+    let test = binding2.to_str().expect("REASON");
+    copy_dir_all(out_path, binding2).expect("Could not copy");
     gates::generate();
     poseidon_constants::generate();
     #[cfg(target_os = "macos")]
